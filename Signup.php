e7fc831a76ce74f2d83e35d6a10292ad0d0d81c0
kaushik k <52545122+KaushikKodeeswaran@users.noreply.github.com>
2021-01-12 17:04:33 +0530

    Add files via upload

---
+++ Signup.php
@@ -0,0 +1,51 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="signup.css">
+    <title> Product deal India</title>
+    <p><h1>Product Deal India &nbsp </h1></p>
+    <p style="color:#ff8000";> TELL US ABOUT YOUR BUSINESS </p><br>
+    <hr height="30%">
+  </head>
+  <body>
+    <img class="relative" src="shop1.png" alt="Error Loading image" width="200"
+         height="140">
+    <form>
+      <br>Store Name <br><input type="text" name="name"><br>
+      select Product category<br>
+      <select name="category" >
+        <option value=""></option>
+        <option value="ELECTRONICS">ELECTRONICS</option>
+        <option value="SPORTS">SPORTS</option>
+        <option value="CLOTHING">CLOTHING</option>
+        <option value="HOME&FURNITURE">HOME&FURNITURE</option>
+        <option value="BOOKS">BOOKS</option>
+      </select>
+      <br>
+      <hr>
+      <br>Street Address <br><input type="text" name="Street1"><br>
+      Street Address Line 2<br><input type="text" name="Street2"><br>
+      City <br><input type="text" name="city"><br>
+      State <br><input type="text" name="name"><br>
+      Country<br>
+      <select name="country" >
+        <option value=""></option>
+        <option value="AFGHANISTAN">AFGHANISTAN</option>
+        <option value="ARMENIA">ARMENIA</option>
+        <option value="AZERBAIJAN">AZERBAIJAN</option>
+        <option value="BAHRAIN">BAHRAIN</option>
+        <option value="BANGLADESH">BANGLADESH</option>
+        <option value="BHUTAN">BHUTAN</option>
+        <option value="BRUNEI">BRUNEI</option>
+        <option value="CAMBODIA">CAMBODIA</option>
+        <option value="CHINA">CHINA</option>
+        <option value="CYPRUS">CYPRUS</option>
+    </select><br>
+      Zip code <br><input type="text" name="name"><br>
+      Phone Number <br><input type="text" name="name"><br>
+      Email Address <br><input type="text" name="contactname"><br>
+
+      <hr width>
+
+    </form>
+  </body>
+</html>
